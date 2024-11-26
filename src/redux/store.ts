343b6ea5bc990df4667e637f99cf5995198c923f
BKSoftwares21 <benitakazadi56@gmail.com>
2024-11-26 06:14:25 +0200

    making the structure of the Mind Matrix Ai coach

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/userSlice';
+import metricsReducer from './features/metricsSlice';
+
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+        metrics: metricsReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
